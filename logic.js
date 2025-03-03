4d9bfe9e7565c010163450120609c339f9f6201c
akashRaanjan12 <akash12ranjan@gmail.com>
2025-03-03 15:18:36 +0530

    personal portfolio

---
+++ logic.js
@@ -0,0 +1,75 @@
+var date = new Date();
+document.querySelector(".date").innerHTML = date.getFullYear();
+// circle on the name class
+const cursor = document.querySelector(".cursor");
+const nameBox = document.querySelector(".name");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = `${e.pageX}px`;
+  cursor.style.top = `${e.pageY}px`;
+});
+
+nameBox.addEventListener("mouseenter", () => {
+  cursor.style.opacity = 1; // Show orange cursor
+});
+
+nameBox.addEventListener("mouseleave", () => {
+  cursor.style.opacity = 0; // Hide cursor outside
+});
+
+// 2nd circle on the name class
+const nameBox1 = document.querySelector(".name1");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = `${e.pageX}px`;
+  cursor.style.top = `${e.pageY}px`;
+});
+
+nameBox1.addEventListener("mouseenter", () => {
+  cursor.style.opacity = 1; // Show orange cursor
+});
+
+nameBox1.addEventListener("mouseleave", () => {
+  cursor.style.opacity = 0; // Hide cursor outside
+});
+var section = document.querySelectorAll("section");
+
+window.onscroll = () => {
+  section.forEach((element) => {
+    var top = window.scrollY;
+    var offset = element.offsetTop - 340;
+    var height = element.offsetHeight;
+    if (top >= offset && top < offset + height) {
+      element.classList.add("show");
+    } else {
+      element.classList.remove("show");
+    }
+  });
+};
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.querySelector(".sec-1");
+  container.classList.add("show");
+});
+// Remove the event listener to prevent further animations
+document.removeEventListener("DOMContentLoaded", arguments.collee);
+
+// function vis() {
+//   document.querySelector(".bug-rep-cont").style.display = "flex";
+// }
+function vis() {
+  let bugRepCont = document.querySelector(".bug-rep-cont");
+  let cut = document.querySelector(".cross");
+  let bug = document.querySelector(".BugButton");
+  if (bug && bugRepCont) {
+    // bugRepCont.style.display = "flex";
+    bug.addEventListener("click", function () {
+      bugRepCont.style.display = "flex";
+    });
+  }
+  if (cut && bugRepCont) {
+    // bugRepCont.style.display = "none";
+    cut.addEventListener("click", function () {
+      bugRepCont.style.display = "none";
+    });
+  }
+}
